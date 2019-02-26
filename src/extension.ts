41832a0de46eec370f4d494f414c4686377f2d85
hanjoosten <han.joosten.han@gmail.com>
2019-02-26 16:31:19 +0100

    cleaning up tests

--- src/extension.ts
+++ src/extension.ts
@@ -1,104 +1,22 @@
 import * as vscode from 'vscode'; 
-import {window, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
 
-export function activate(ctx: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error).
     // This line of code will only be executed once when your extension is activated.
     console.log('Congratulations, your extension "Ampersand" is now active!');
     
-    // vscode.languages.setLanguageConfiguration('ampersand', {
-    //     indentationRules: {
-    //         // ^.*\{[^}"']*$
-    //         increaseIndentPattern: vscode.workspace.getConfiguration('ampersand').indentationRules.enabled
-    //             ? /(\bif\b(?!')(.(?!then))*|\b(then|else|m?do|of|let|in|where)\b(?!')|=|->|>>=|>=>|=<<|(^(data)( |\t)+(\w|')+( |\t)*))( |\t)*$/
-    //             : null,
-    //         decreaseIndentPattern: null
-    //     },
-    //     wordPattern: /([\w'_][\w'_\d]*)|([0-9]+\.[0-9]+([eE][+-]?[0-9]+)?|[0-9]+[eE][+-]?[0-9]+)/
-    // });
-    let wordCounter = new WordCounter();
-    let controller = new WordCounterController(wordCounter);
+    // The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
 
-    // add to a list of disposables which are disposed when this extension
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hallo Ampersand!');
+	});
+
+	// add to a list of disposables which are disposed when this extension
     // is deactivated again.
-    ctx.subscriptions.push(controller);
-    ctx.subscriptions.push(wordCounter);
-}
-export class WordCounter {
-
-    private _statusBarItem!: StatusBarItem;
-
-    public updateWordCount() {
-        
-        // Create as needed
-        if (!this._statusBarItem) {
-            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-        } 
-
-        // Get the current text editor
-        let editor = window.activeTextEditor;
-        if (!editor) {
-            this._statusBarItem.hide();
-            return;
-        }
-
-        let doc = editor.document;
-
-        // Only update status if an MD file
-        if (doc.languageId === "ampersand") {
-            let wordCount = this._getWordCount(doc);
-
-            // Update the status bar
-            this._statusBarItem.text = wordCount !== 1 ? `$(pencil) ${wordCount} Words` : '$(pencil) 1 Word';
-            this._statusBarItem.show();
-        } else {
-            this._statusBarItem.hide();
-        }
-    }
-
-    public _getWordCount(doc: TextDocument): number {
-        let docContent = doc.getText();
-
-        // Parse out unwanted whitespace so the split is accurate
-        docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
-        docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-        let wordCount = 0;
-        if (docContent != "") {
-            wordCount = docContent.split(" ").length;
-        }
-
-        return wordCount;
-    }
-
-    public dispose() {
-        this._statusBarItem.dispose();
-    }
-}
-
-class WordCounterController {
-
-    private _wordCounter: WordCounter;
-    private _disposable: Disposable;
-
-    constructor(wordCounter: WordCounter) {
-        this._wordCounter = wordCounter;
-        this._wordCounter.updateWordCount();
-
-        // subscribe to selection change and editor activation events
-        let subscriptions: Disposable[] = [];
-        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
-        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
-
-        // create a combined disposable from both event subscriptions
-        this._disposable = Disposable.from(...subscriptions);
-    }
-
-    private _onEvent() {
-        this._wordCounter.updateWordCount();
-    }
-
-    public dispose() {
-        this._disposable.dispose();
-    }
+    context.subscriptions.push(disposable);
 }
