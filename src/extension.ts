2c70a3f835013ed88ac718de301fbadbbeba00ae
hanjoosten <han.joosten.han@gmail.com>
2019-02-26 17:14:14 +0100

    checkVersion

--- src/extension.ts
+++ src/extension.ts
@@ -9,11 +9,11 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.checkVersion', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hallo Ampersand!');
+		vscode.window.showInformationMessage('Checking the version of ampersand will be available soon!');
 	});
 
 	// add to a list of disposables which are disposed when this extension
