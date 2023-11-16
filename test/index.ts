6e4a4b395f17835465ffa9f406cf27b85329637f
Luc Somers <lucsomers@hotmail.com>
2023-11-16 15:34:00 +0100

    commented configure step because of broken property

--- test/index.ts
+++ test/index.ts
@@ -15,9 +15,9 @@ import * as testRunner from '@vscode/test-electron';
 // You can directly control Mocha options by configuring the test runner below
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options
 // for more info
-testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
-});
+// testRunner.configure({
+//     ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
+//     useColors: true // colored output from test results
+// });
 
 module.exports = testRunner;
