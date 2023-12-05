3c4c12d1b9efd3bab4eedfdf5b1cee2ba002ac25
Fran Slot <Fran.Slot@ordina.nl>
2023-12-05 12:08:11 +0100

    Replace text using buffer

--- src/commands/generatePrototypeCommand.ts
+++ src/commands/generatePrototypeCommand.ts
@@ -1,5 +1,7 @@
 import { fileUtils, terminalUtils } from "../utils";
 import * as fs from 'fs';
+import * as vscode from 'vscode';
+import * as Buffer from 'buffer';
 
 export class generatePrototypeCommand {
     static GeneratePrototypeCommand()
@@ -12,8 +14,30 @@ export class generatePrototypeCommand {
         const fileContent : string = fs.readFileSync(currentActiveFilePath, 'utf-8');
         const encodedContent : string = btoa(fileContent);
         
-        const manifestContent : string = fs.readFileSync('/workspaces/Ampersand-Language-Support/src/prototype-template.yaml', 'utf-8');
-        manifestContent.replace('{{scriptContent}}', encodedContent);
+        // const manifestContent : string = fs.readFileSync('/workspaces/Ampersand-Language-Support/src/prototype-template.yaml', 'utf-8');
+        // manifestContent.replace('{{scriptContent}}', encodedContent);
+
+        let fileUri = vscode.Uri.file('/workspaces/Ampersand-Language-Support/src/prototype-template.yaml');
+
+        // read the file contents as a Uint8Array
+        vscode.workspace.fs.readFile(fileUri).then(data => {
+
+          // convert the Uint8Array to a string
+          let text = Buffer.Buffer.from(data).toString();
+        
+          // replace the text with the new value
+          let newText = text.replace('{{scriptContent}}', 'something else');
+        
+          // convert the string to a Uint8Array
+          let newData = Buffer.Buffer.from(newText);
+        
+          // write the file contents from the Uint8Array
+          vscode.workspace.fs.writeFile(fileUri, newData).then(() => {
+
+            // show a message that the file was updated
+            vscode.window.showInformationMessage('File updated');
+          });
+        });
 
         // terminalUtils.RunCommandInNewTerminal("ampersand generate atlas",
         // `ampersand population --output-dir='./' --build-recipe Grind --output-format json --verbosity warn ${currentActiveFilePath}`)
