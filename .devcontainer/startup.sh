8b9904213125d040b4542fed8662ff5e1daa3a0e
Fran Slot <Fran.Slot@ordina.nl>
2023-12-05 11:05:02 +0100

    Added shell script

---
+++ .devcontainer/startup.sh
@@ -0,0 +1,11 @@
+set -e
+
+npm install
+npm install --global @vscode/vsce
+npm install --global typescript
+npm install @types/node
+
+git restore .
+
+vsce package 0.0.0
+code --install-extension language-ampersand-0.0.0.vsix
