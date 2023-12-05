48f1b9f602cdd4d1760ae19d62df4a705c90d97e
Fran Slot <Fran.Slot@ordina.nl>
2023-12-05 11:49:31 +0100

    Changed template path to full path

--- src/commands/generatePrototypeCommand.ts
+++ src/commands/generatePrototypeCommand.ts
@@ -11,8 +11,8 @@ export class generatePrototypeCommand {
 
         const fileContent : string = fs.readFileSync(currentActiveFilePath, 'utf-8');
         const encodedContent : string = btoa(fileContent);
-
-        const manifestContent : string = fs.readFileSync('src/prototype-template.yaml', 'utf-8');
+        
+        const manifestContent : string = fs.readFileSync('/workspaces/Ampersand-Language-Support/src/prototype-template.yaml', 'utf-8');
         manifestContent.replace('{{scriptContent}}', encodedContent);
 
         // terminalUtils.RunCommandInNewTerminal("ampersand generate atlas",
