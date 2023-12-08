6478b4efedd9e8c4c5f597b384e1caa907ac2152
Fran Slot <Fran.Slot@ordina.nl>
2023-12-08 08:52:43 +0100

    Added flags

--- assets/kubernetes.sh
+++ assets/kubernetes.sh
@@ -1,13 +1,18 @@
 set -e
 
-FILENAME=$1
-DEPLOYMENT=$2
-SERVICE=$3
+while getopts f:d:s: flag
+do
+    case "${flag}" in
+        f) filename=${OPTARG};;
+        d) deployment=${OPTARG};;
+        s) service=${OPTARG};;
+    esac
+done
 
-kubectl apply -f $FILENAME
+kubectl apply -f $filename
 
-while [[ $(kubectl get deployment $DEPLOYMENT -o 'jsonpath={..status.conditions[?(@.type=="Available")].status}') != "True" ]];
+while [[ $(kubectl get deployment $deployment -o 'jsonpath={..status.conditions[?(@.type=="Available")].status}') != "True" ]];
 do echo "waiting for deployment" && sleep 1;
 done
 
-kubectl port-forward svc/$SERVICE -n default 8000:80
+kubectl port-forward svc/$service -n default 8000:80
