54d37db9d2933ae74e1055dc90c03f2e33674908
lucsomers <luc.somers@ordina.nl>
2024-02-27 14:01:42 +0100

    Added initializer for array

--- src/extension.ts
+++ src/extension.ts
@@ -6,12 +6,12 @@ import { ampersandVersionChecker } from './ampersand';
 import { constants } from './constants';
 import { checkVersionCommand, generateAtlasCommand, generateFunctionalSpecCommand, generatePrototypeCommand } from './commands';
 
-let commands: Array<ICommand>;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+    
+    let commands: Array<ICommand> = new Array<ICommand>();
     // Use the console to output diagnostic information (console.log) and errors (console.error).
     // This line of code will only be executed once when your extension is activated.
     console.info(
