88dfb0dff5b4bea574d86c2d90e4bc34d6225ffd
hanjoosten <han.joosten.han@gmail.com>
2025-01-01 22:57:06 +0000

    Restore proper version numbering

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -6,12 +6,3 @@ npm install --global @vscode/vsce
 npm install --global typescript
 npm install @types/node
 npm i --global esbuild
-
-# git restore .
-
-# npm run esbuild
-# vsce package 0.0.0
-# echo "Aap"
-# code --version
-# echo "Noot"
-# code --install-extension language-ampersand-0.0.0.vsix
