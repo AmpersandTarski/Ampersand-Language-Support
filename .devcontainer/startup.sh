d0e610cd36cac9762e09e1e4962000acf93e0da6
hanjoosten <han.joosten.han@gmail.com>
2025-01-01 21:21:10 +0000

    Update dependencies and devcontainer stuff

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,13 +1,17 @@
+# Exit immediately if a command exits with a non-zero status.
 set -e
 
-npm install
+npm install --global npm@latest
 npm install --global @vscode/vsce
 npm install --global typescript
 npm install @types/node
 npm i --global esbuild
 
-git restore .
+# git restore .
 
-npm run esbuild
-vsce package 0.0.0
-code --install-extension language-ampersand-0.0.0.vsix
+# npm run esbuild
+# vsce package 0.0.0
+# echo "Aap"
+# code --version
+# echo "Noot"
+# code --install-extension language-ampersand-0.0.0.vsix
