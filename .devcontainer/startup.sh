c09e472686ea4cd55ae04e87a5714c9ab39d6719
Han Joosten <han.joosten.han@gmail.com>
2025-07-14 05:40:39 +0000

    modification to devcontainer

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,6 +1,8 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+npm update --location=global npm
+
 npm install --global npm@latest
 npm install --global @vscode/vsce
 npm install --global typescript
